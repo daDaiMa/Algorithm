955d9a18b5fdb3a52c5114b9f4a6ebc067dc54ef
sudo rm -rf kk <17606706701@163.com>
2019-07-06 10:25:56 +0800

    2019-07-06 10:25:56

---
+++ tencent contains duplicate.go
@@ -0,0 +1,13 @@
+
+func containsDuplicate(nums []int) bool {
+    if len(nums)<=1{
+        return false
+    }
+	sort.Ints(nums)
+	for i:=1;i<len(nums);i++{
+		if nums[i]==nums[i-1]{
+			return true
+		}
+	}
+	return false
+} 
