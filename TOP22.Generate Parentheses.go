b39439a2a0ff9e646e215ca3e53d4dc880a11633
sudo rm -rf kk <17606706701@163.com>
2019-07-11 17:10:56 +0800

    之前的错了、换了一个新方法  2019-07-11 17:10:56

--- TOP22.Generate Parentheses.go
+++ TOP22.Generate Parentheses.go
@@ -1,24 +1,46 @@
 
-var ans []string
-var m map[string]bool
-
 func generateParenthesis(n int) []string {
-	m = make(map[string]bool)
-	recursive(n, "", "")
+	ans := make([]string, 0)
+	for i := MIN(n); i <= MAX(n); i++ {
+		if ok, str := Check(n*2, i); ok {
+			//reversal(str)
+			ans = append(ans, string(str))
+		}
+	}
 	return ans
 }
 
-func recursive(d int, pre, bck string) {
-	if d == 0 {
-		str := pre + bck
-		if ok, _ := m[str]; !ok {
-			ans = append(ans, pre+bck)
-			m[str] = true
-		}
-		return
+func MAX(n int) int {
+	return 1<<uint(2*n) - 1<<uint(n)
+}
+
+func MIN(n int) int {
+	min := 0
+	for i := 0; i < n*2; i += 2 {
+		min += 1 << uint(i+1)
 	}
-	d--
-	recursive(d, pre+"(", ")"+bck)
-	recursive(d, pre+"()", bck)
-	recursive(d, pre, "()"+bck)
+	return min
+}
+
+func Check(size, n int) (bool, []byte) {
+	var buffer bytes.Buffer
+	weight := 0
+	base := 1 << uint(size-1)
+	for {
+		if n&base == base {
+			weight += 1
+			buffer.WriteByte('(')
+		} else {
+			weight -= 1
+			buffer.WriteByte(')')
+			if weight < 0 {
+				return false, []byte{}
+			}
+		}
+		n <<= 1
+		size--
+		if size == 0 {
+			return weight == 0, buffer.Bytes()
+		}
+	}
 }
