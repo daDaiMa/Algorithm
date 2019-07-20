47d37233ce5fc758c8e42eee199287e84332d096
sudo rm -rf kk <17606706701@163.com>
2019-07-21 00:53:06 +0800

    优化

--- TOP127.WordLadder.cpp
+++ TOP127.WordLadder.cpp
@@ -1,77 +1,32 @@
-
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string> &wordList) {
-
-        bool ok = false;
-        for (int i = 0; i < wordList.size(); i++) {
-            if (wordList[i] == endWord) {
-                ok = true;
-                break;
-            }
-        }
-        if (!ok) {
-            return 0;
-        }
-
-        map<string, int> vis1;
-        map<string, int> vis2;
-        vis1[beginWord] = 1;
-        vis2[endWord] = 1;
-        queue<string> q1;
-        queue<string> q2;
-        q1.push(beginWord);
-        q2.push(endWord);
-//        map<string, bool> check;
-//        for (int i = 0; i < wordList.size(); i++) {
-//            check[wordList[i]] = true;
-//        }
-
-        while (!q1.empty() && !q2.empty()) {
-            //先q1
-            string tmp = q1.front();
-            q1.pop();
-            if (vis2.find(tmp) != vis2.end()) {
-                return vis1[tmp] + vis2[tmp];
-            }
-            for (int i = 0; i < wordList.size(); i++) {
-                if (vis1.find(wordList[i]) != vis1.end())continue;
-
-                int count = 0;
-                for (int j = 0; j < tmp.length(); j++) {
-                    if (tmp[j] != wordList[i][j])count++;
-                    if (count == 2) {
-                        break;
+        unordered_set<string> s1{beginWord};
+        unordered_set<string> s2{endWord};
+        unordered_set<string> m(wordList.begin(), wordList.end());
+        if(!m.count(endWord))return 0;
+        int word_length = beginWord.length();
+        int step = 0;
+        while (!s1.empty() && !s2.empty()) {
+            ++step;
+            //从少的开始扩展
+            unordered_set<string> s;
+            if (s1.size() > s2.size()) swap(s1, s2);
+            for (string w:s1) {
+                for (int i = 0; i < word_length; i++) {
+                    char ori = w[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        w[i] = c;
+                        if (s2.count(w))return step + 1;
+                        if (!m.count(w))continue;
+                        s.insert(w);
+                        m.erase(w);
                     }
-                }
-                if (count == 1) {
-                    if (vis2.find(wordList[i]) != vis2.end())return vis2[wordList[i]] + +vis1[tmp];
-                    q1.push(wordList[i]);
-                    vis1[wordList[i]] = vis1[tmp] + 1;
+                    w[i] = ori;
                 }
             }
-            tmp = q2.front();
-            q2.pop();
-            if (vis1.find(tmp) != vis1.end()) {
-                return vis1[tmp] + vis2[tmp];
-            }
-            for (int i = 0; i < wordList.size(); i++) {
-                if (vis2.find(wordList[i]) != vis2.end())continue;
-                int count = 0;
-                for (int j = 0; j < tmp.length(); j++) {
-                    if (tmp[j] != wordList[i][j])count++;
-                    if (count == 2) {
-                        break;
-                    }
-                }
-                if (count == 1) {
-                    if (vis1.find(wordList[i]) != vis1.end())return vis1[wordList[i]] + vis2[tmp];
-                    q2.push(wordList[i]);
-                    vis2[wordList[i]] = vis2[tmp] + 1;
-                }
-            }
+            swap(s, s1);
         }
-
         return 0;
     }
 };
