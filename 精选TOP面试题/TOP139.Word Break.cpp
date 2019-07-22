9325b5940cb533540b0bd13e4c91f65872054db5
sudo rm -rf kk <17606706701@163.com>
2019-07-22 17:52:31 +0800

    2019-07-22 17:52:31

---
+++ 精选TOP面试题/TOP139.Word Break.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+    set<string> _set;
+    set<int> _size;
+    map<string, bool> ok;
+    int *size;
+
+
+    bool wordBreak(string s, vector<string> &wordDict) {
+        this->_set.clear();
+        for (string str:wordDict) {
+            this->_set.insert(str);
+            this->_size.insert(str.length());
+        }
+        this->size = new int[this->_size.size()];
+        int index = this->_size.size() - 1;
+        for (auto it = this->_size.begin(); it != this->_size.end(); it++) {
+            this->size[index--] = *it;
+        }
+        return dfs(s);
+    }
+
+    bool dfs(string str) {
+        if (str.length() == 0)return true;
+        if (ok.count(str)) {
+//            cout << "hhh";
+            return ok[str];
+        }
+        for (int i = this->_size.size() - 1; i >= 0; i--) {
+            if (this->size[i] > str.length())continue;
+            if (_set.count(str.substr(0, this->size[i]))) {
+                if (dfs(str.substr(this->size[i]))) {
+                    ok[str] = true;
+                    return true;
+                }
+                ok[str] = false;
+            }
+        }
+        return false;
+    }
+};
