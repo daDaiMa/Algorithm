43578c9d2d86a2f8d6df86ea9e644f38567a6aaf
sudo rm -rf kk <17606706701@163.com>
2019-07-26 11:28:17 +0800

    一个map还是有点慢  2019-07-26 11:28:17

--- 精选TOP面试题/TOP454.4Sum II.cpp
+++ 精选TOP面试题/TOP454.4Sum II.cpp
@@ -3,18 +3,15 @@ public:
     int fourSumCount(vector<int> &A, vector<int> &B, vector<int> &C, vector<int> &D) {
         // 先A和B合并，C和D合并
         map<int,int> AB;
-        map<int,int> CD;
         for (int i = 0; i < A.size(); i++) {
             for (int j = 0; j < B.size(); j++) {
                 AB[A[i] + B[j]]++;
-                CD[C[i] + D[j]]++;
             }
         }
         long long ans = 0;
-        for(auto it=AB.begin();it!=AB.end();it++){
-            if(CD.count(0-it->first)){
-                ans+=CD[0-it->first]*it->second;
-            }
+        for(auto c:C)
+        for(auto d:D){
+            ans+=AB[0-c-d];
         }
         return ans;
     }
