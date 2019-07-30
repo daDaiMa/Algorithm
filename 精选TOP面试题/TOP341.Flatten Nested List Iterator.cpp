82fea5e4018a306c5ae45cfcc3db6d1f20d5a847
sudo rm -rf kk <17606706701@163.com>
2019-07-30 23:48:44 +0800

    OK  2019-07-30 23:48:44

--- 精选TOP面试题/TOP341.Flatten Nested List Iterator.cpp
+++ 精选TOP面试题/TOP341.Flatten Nested List Iterator.cpp
@@ -17,115 +17,30 @@
  */
 class NestedIterator {
 public:
-    vector<NestedInteger>ls;
-    vector<int>index;
-    bool has;
+    int index;
+    vector<int>ans;
     NestedIterator(vector<NestedInteger> &nestedList) {
-        has=nestedList.size()!=0;
-        if(!has)return;
-        index.push_back(0);
-        ls.assign(nestedList.begin(),nestedList.end());
-        NestedInteger n=ls[0];
-        while(!n.isInteger()){
-            if(n.getList().size()==0){
-                has=false;
-                return;
-            }
-            n=n.getList()[0];
-            index.push_back(0);
-        }
+       for(auto it:nestedList){
+            push(it);
+       }
+       index=0;
     }
 
     int next() {
-        // 先寻找
-        int ans;
-        
-        cout<<"查找时:";
-        print();
-        
-        
-        NestedInteger n=ls[index[0]];
-        for(int i=1;i<index.size();i++)n=n.getList()[index[i]];
-        ans=n.getInteger();
-        cout<<"答案:"<<ans<<endl;
-
-        while(true){
-            index[index.size()-1]++;
-            if(index[0]>=ls.size()){
-                has=false;
-                break;
-            }
-            n=ls[index[0]];
-            if(n.isInteger())break;                
-            int i;
-            
-
-            cout<<"递增:";
-            print();
-            
-            
-            
-            for(i=1;i<index.size()-1;i++)n=n.getList()[index[i]];
-            // cout<<"1"<<endl;
-            
-            if(index.size()==1){
-                bool flag=false;
-                while(!n.isInteger()){
-                    if(n.getList().size()==0){
-                        flag=true;
-                        break;
-                    }
-                    n=n.getList()[0];
-                    index.push_back(0);
-                }
-                if(flag)continue;
-                break;
-            }
-            
-            if(n.getList().size()<=index[i]){
-                // cout<<"2"<<endl;
-                index.pop_back();
-                
-                cout<<"越界:";
-                print();
-                continue;
-            }else{
-                bool flag=false;
-                // cout<<"3"<<endl;
-                n=n.getList()[index[i]];
-                // cout<<"4"<<endl;
-                while(!n.isInteger()){
-                    if(n.getList().size()==0){
-                        flag=true;
-                        break;
-                    }
-                    n=n.getList()[0];
-                    // cout<<"5"<<endl;
-                    index.push_back(0);
-                }
-                 cout<<"变动:";
-                print();
-                if(flag)continue;
-                break;
-               // cout<<"6"<<endl;
-            }
-           
-        }
-        
-         
-        
-        
-        return ans;
-    }   
+       return ans[index++]; 
+    }
 
     bool hasNext() {
-        return has;
+       return index<ans.size(); 
     }
-    
-    void print(){
-        for(int i:index){
-            cout<<i<<",";
-        }cout<<endl;
+
+    void push(NestedInteger n){
+        if(n.isInteger())ans.push_back(n.getInteger());
+        else{
+            for(auto it:n.getList()){
+                push(it);
+            }
+        }
     }
 };
 
