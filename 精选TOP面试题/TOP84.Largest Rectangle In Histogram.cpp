3eb079ea0e4a40375e9452c174e90e9c3cbb0ed9
sudo rm -rf kk <17606706701@163.com>
2019-08-04 11:28:05 +0800

    减枝*2   2019-08-04 11:28:05

--- 精选TOP面试题/TOP84.Largest Rectangle In Histogram.cpp
+++ 精选TOP面试题/TOP84.Largest Rectangle In Histogram.cpp
@@ -5,6 +5,7 @@ public:
         int ans=0;
         for(int i=0;i<heights.size();i++){
             int _min=heights[i]; 
+            if(i&&heights[i]<=heights[i-1])continue;
             for(int j=i;j<heights.size();j++){
                _min=min(_min,heights[j]);
                if(ans>(heights.size()-i)*_min)break;
