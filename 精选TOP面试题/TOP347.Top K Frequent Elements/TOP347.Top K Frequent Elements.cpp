83c8734eb244cf1abeb0e0c14360cd8da1674503
sudo rm -rf kk <17606706701@163.com>
2019-08-10 21:17:09 +0800

    fix

--- 精选TOP面试题/TOP347.Top K Frequent Elements/TOP347.Top K Frequent Elements.cpp
+++ 精选TOP面试题/TOP347.Top K Frequent Elements/TOP347.Top K Frequent Elements.cpp
@@ -1,20 +1,19 @@
-
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int>m;
-        for(auto it =nums.begin();it!=nums.end();it++){
+    vector<int> topKFrequent(vector<int> &nums, int k) {
+        map<int, int> m;
+        for (auto it = nums.begin(); it != nums.end(); it++) {
             m[*it]++;
         }
-        vector<pair<int,int>>tmp;
-        vector<int>ans;
-        for(auto it=m.begin();it!=m.end();it++){
-            tmp.push_back(make_pair(it->first,it->second));
+        vector<pair<int, int>> tmp;
+        vector<int> ans;
+        for (auto it = m.begin(); it != m.end(); it++) {
+            tmp.push_back(make_pair(it->first, it->second));
         }
-        sort(tmp.begin(),tmp.end(),[](const pair<int,int>&a,const pair<int,int>&b){
-                        return a->second>b->second;
-                        });
-        for(int i=0;i<k;i++){
+        sort(tmp.begin(), tmp.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+            return a.second > b.second;
+        });
+        for (int i = 0; i < k; i++) {
             ans.push_back(tmp[i].first);
         }
         return ans;
