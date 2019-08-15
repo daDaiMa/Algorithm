587e501ce0dc32a9ebefbd786d91defe1a2e17d8
sudo rm -rf kk <17606706701@163.com>
2019-08-15 15:21:01 +0800

    卡特兰数 🤯

--- leetcode热题HOT100/98.Unique Binary Search Trees.cpp
+++ leetcode热题HOT100/98.Unique Binary Search Trees.cpp
@@ -1,6 +1,14 @@
+/*
+ * 卡特兰数
+ * 🤯
+ */
 class Solution {
 public:
     int numTrees(int n) {
-        
+        long long c=1;
+        for(int i=0;i<n;i++){
+            c=c*2*(2*i+1)/(i+2);
+        }
+        return (int)c;
     }
 };
