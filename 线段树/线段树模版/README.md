55ed62a4451f5af80009bf5ba5d214b741149840
sudo rm -rf kk <17606706701@163.com>
2019-10-09 14:37:26 +0800

    线段树懒更新

--- 线段树/线段树模版/README.md
+++ 线段树/线段树模版/README.md
@@ -5,6 +5,8 @@
 ![image-20191008151044515](README.assets/image-20191008151044515.png)
 
 ```c++
+// 这种当数据规模大的时候 会超时 还会溢出
+
 #include <bits/stdc++.h>
 
 #define MAXN 100005
@@ -93,6 +95,132 @@ int main() {
     }
 
     return 0;
+}
+```
+
+```cpp
+// 采用懒更新策略
+#include <bits/stdc++.h>
+
+#define MAXN 100005
+using namespace std;
+
+inline int ls(int p) { return p << 1; }
+
+inline int rs(int p) { return p << 1 | 1; }
+
+struct node {
+  int l, r, update;
+  long long sum;
+  node() {}
+  node(int l, int r, long long sum) : l(l), r(r), sum(sum) { update = 0; }
+};
+
+node s[MAXN * 4 + 2];
+
+int nums[MAXN];
+int N, M;
+
+void build(int arr[], int root, int l, int r) {
+  s[root].l = l;
+  s[root].r = r;
+  if (l == r) {
+    s[root].sum = arr[l];
+    return;
+  }
+  int mid = (r + l) >> 1;
+  build(arr, ls(root), l, mid);
+  build(arr, rs(root), mid + 1, r);
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+}
+
+void push_down(int root) {
+  s[ls(root)].update += s[root].update;
+  s[ls(root)].sum += (s[ls(root)].r - s[ls(root)].l + 1) * s[root].update;
+  s[rs(root)].update += s[root].update;
+  s[rs(root)].sum += (s[rs(root)].r - s[rs(root)].l + 1) * s[root].update;
+  s[root].update = 0;
+}
+
+// 懒更新版本
+void add(int root, int l, int r, int x) {
+  if (s[root].l >= l && s[root].r <= r) {
+    s[root].sum += (r - l + 1) * x;
+    s[root].update += x;
+    return;
+  }
+  push_down(root);
+  int mid = (s[root].l + s[root].r) >> 1;
+  if (l > mid)
+    add(rs(root), l, r, x);
+  else if (r <= mid)
+    add(ls(root), l, r, x);
+  else {
+    add(ls(root), l, mid, x);
+    add(rs(root), mid + 1, r, x);
+  }
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+}
+
+/*
+原来的版本
+void add(int root, int l, int r, int x) {
+  if (s[root].l == l && s[root].l == s[root].r) {
+    s[root].sum += x;
+    return;
+  }
+  int mid = (s[root].l + s[root].r) >> 1;
+  if (l > mid)
+    add(rs(root), l, r, x);
+  else if (r <= mid)
+    add(ls(root), l, r, x);
+  else {
+    add(ls(root), l, mid, x);
+    add(rs(root), mid + 1, r, x);
+  }
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+}
+*/
+
+long long find(int root, int l, int r) {
+  if (l > s[root].r || r < s[root].l)
+    return 0;
+  if (l <= s[root].l && r >= s[root].r) {
+    return s[root].sum;
+  }
+  push_down(root);
+  int mid = (s[root].l + s[root].r) / 2;
+  if (r <= mid)
+    return find(ls(root), l, r);
+  if (l > mid)
+    return find(rs(root), l, r);
+  return find(ls(root), l, mid) + find(rs(root), mid + 1, r);
+}
+
+int main() {
+
+  if (!freopen("../input.txt", "r", stdin)) {
+    printf("open failed");
+    exit(1);
+  }
+
+  scanf("%d%d", &N, &M);
+  for (int i = 1; i <= N; i++)
+    scanf("%d", &nums[i]);
+  build(nums, 1, 1, N);
+  while (M--) {
+    int op, x, y, k;
+    scanf("%d", &op);
+    if (op == 1) {
+      scanf("%d%d%d", &x, &y, &k);
+      add(1, x, y, k);
+    } else {
+      scanf("%d%d", &x, &y);
+      printf("%lld\n", find(1, x, y));
+    }
+  }
+
+  return 0;
 }
 ```
 
--- 线段树/线段树模版/main.cpp
+++ 线段树/线段树模版/main.cpp
@@ -8,12 +8,10 @@ inline int ls(int p) { return p << 1; }
 inline int rs(int p) { return p << 1 | 1; }
 
 struct node {
-    int l, r, sum;
-
-    //  node *left, *right;
-    node() {}
-
-    node(int l, int r, int sum) : l(l), r(r), sum(sum) {}
+  int l, r, update;
+  long long sum;
+  node() {}
+  node(int l, int r, long long sum) : l(l), r(r), sum(sum) { update = 0; }
 };
 
 node s[MAXN * 4 + 2];
@@ -22,68 +20,101 @@ int nums[MAXN];
 int N, M;
 
 void build(int arr[], int root, int l, int r) {
-    s[root].l = l;
-    s[root].r = r;
-    if (l == r) {
-        s[root].sum = arr[l];
-        return;
-    }
-    int mid = (r + l) >> 1;
-    build(arr, ls(root), l, mid);
-    build(arr, rs(root), mid + 1, r);
-    s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+  s[root].l = l;
+  s[root].r = r;
+  if (l == r) {
+    s[root].sum = arr[l];
+    return;
+  }
+  int mid = (r + l) >> 1;
+  build(arr, ls(root), l, mid);
+  build(arr, rs(root), mid + 1, r);
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+}
+
+void push_down(int root) {
+  s[ls(root)].update += s[root].update;
+  s[ls(root)].sum += (s[ls(root)].r - s[ls(root)].l + 1) * s[root].update;
+  s[rs(root)].update += s[root].update;
+  s[rs(root)].sum += (s[rs(root)].r - s[rs(root)].l + 1) * s[root].update;
+  s[root].update = 0;
 }
 
 void add(int root, int l, int r, int x) {
-    if (s[root].l == l && s[root].l == s[root].r) {
-        s[root].sum += x;
-        return;
-    }
-    int mid = (s[root].l + s[root].r) >> 1;
-    if (l > mid)
-        add(rs(root), l, r, x);
-    else if (r <= mid)
-        add(ls(root), l, r, x);
-    else {
-        add(ls(root), l, mid, x);
-        add(rs(root), mid + 1, r, x);
-    }
-    s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+  if (s[root].l >= l && s[root].r <= r) {
+    s[root].sum += (r - l + 1) * x;
+    s[root].update += x;
+    return;
+  }
+  push_down(root);
+  int mid = (s[root].l + s[root].r) >> 1;
+  if (l > mid)
+    add(rs(root), l, r, x);
+  else if (r <= mid)
+    add(ls(root), l, r, x);
+  else {
+    add(ls(root), l, mid, x);
+    add(rs(root), mid + 1, r, x);
+  }
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
 }
 
-int find(int root, int l, int r) {
-    if (l > s[root].r || r < s[root].l)
-        return 0;
-    if (l <= s[root].l && r >= s[root].r) {
-        return s[root].sum;
-    }
-    int mid = (s[root].l + s[root].r) / 2;
-    if (r <= mid)
-        return find(ls(root), l, r);
-    if (l > mid)
-        return find(rs(root), l, r);
-    return find(ls(root), l, mid) + find(rs(root), mid + 1, r);
+/*
+void add(int root, int l, int r, int x) {
+  if (s[root].l == l && s[root].l == s[root].r) {
+    s[root].sum += x;
+    return;
+  }
+  int mid = (s[root].l + s[root].r) >> 1;
+  if (l > mid)
+    add(rs(root), l, r, x);
+  else if (r <= mid)
+    add(ls(root), l, r, x);
+  else {
+    add(ls(root), l, mid, x);
+    add(rs(root), mid + 1, r, x);
+  }
+  s[root].sum = s[ls(root)].sum + s[rs(root)].sum;
+}
+*/
+
+long long find(int root, int l, int r) {
+  if (l > s[root].r || r < s[root].l)
+    return 0;
+  if (l <= s[root].l && r >= s[root].r) {
+    return s[root].sum;
+  }
+  push_down(root);
+  int mid = (s[root].l + s[root].r) / 2;
+  if (r <= mid)
+    return find(ls(root), l, r);
+  if (l > mid)
+    return find(rs(root), l, r);
+  return find(ls(root), l, mid) + find(rs(root), mid + 1, r);
 }
 
 int main() {
 
-    // freopen("input.txt", "r", stdin);
+  if (!freopen("../input.txt", "r", stdin)) {
+    printf("open failed");
+    exit(1);
+  }
 
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++)
-        cin >> nums[i];
-    build(nums, 1, 1, N);
-    while (M--) {
-        int op, x, y, k;
-        cin >> op;
-        if (op == 1) {
-            cin >> x >> y >> k;
-            add(1, x, y, k);
-        } else {
-            cin >> x >> y;
-            cout << find(1, x, y) << endl;
-        }
+  scanf("%d%d", &N, &M);
+  for (int i = 1; i <= N; i++)
+    scanf("%d", &nums[i]);
+  build(nums, 1, 1, N);
+  while (M--) {
+    int op, x, y, k;
+    scanf("%d", &op);
+    if (op == 1) {
+      scanf("%d%d%d", &x, &y, &k);
+      add(1, x, y, k);
+    } else {
+      scanf("%d%d", &x, &y);
+      printf("%lld\n", find(1, x, y));
     }
+  }
 
-    return 0;
+  return 0;
 }
