1472916a4e1abddb294519a03038c2ed92c32aff
sudo rm -rf kk <17606706701@163.com>
2019-10-18 23:08:21 +0800

    AC

--- 并查集/修复公路/main.cpp
+++ 并查集/修复公路/main.cpp
@@ -3,72 +3,47 @@
 #define MAXN 1005
 using namespace std;
 
-struct Node {
-    int w, v;
-
-    Node() {}
-};
-
-Node f[MAXN];
-
-int find(int x) {
-    if (f[x].v == x)
-        return x;
-    f[x].v = find(f[x].v);
-    return f[x].v;
-}
+int f[MAXN];
+int find(int x) { return f[x] == x ? x : f[x] = find(f[x]); }
 
 bool join(int a, int b) {
-    int va = find(a);
-    int vb = find(b);
-    if (va == vb)
-        return false;
-    if (f[a].w >= f[b].w) {
-        if (f[a].w == f[b].w)
-            f[a].w += 1;
-        f[b].v = va;
-    } else {
-        f[a].v = vb;
-    }
-    return true;
+  int fa = find(a), fb = find(b);
+  if (fa == fb)
+    return false;
+  f[fa] = fb;
+  return true;
 }
 
 void init() {
-    for (int i = 0; i < MAXN; i++) {
-        f[i].v = i;
-        f[i].w = 0;
-    }
+  for (int i = 0; i < MAXN; i++) {
+    f[i] = i;
+  }
 }
 
 int N, M;
 struct Edge {
-    int s, e, t;
+  int s, e, t;
 };
 
 Edge edge[100005];
 
 int main() {
-    freopen("testdata.in", "r", stdin);
-    cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-        cin >> edge[i].s >> edge[i].e >> edge[i].t;
+  cin >> N >> M;
+  for (int i = 0; i < M; i++) {
+    cin >> edge[i].s >> edge[i].e >> edge[i].t;
+  }
+  sort(edge, edge + M, [](const Edge &a, const Edge &b) { return a.t < b.t; });
+  int left = N;
+  init();
+  for (int i = 0; i < M; i++) {
+    if (join(edge[i].s, edge[i].e)) {
+      left--;
+      if (left == 1) {
+        cout << edge[i].t;
+        return 0;
+      }
     }
-    sort(edge, edge + M, [](const Edge &a, const Edge &b) { return a.t < b.t; });
-//    for (int i = 0; i < M; i++) {
-//        printf("start=%d,end=%d,time=%d\n", edge[i].s , edge[i].e , edge[i].t );
-//    }
-//    return 0;
-    int left = N;
-    init();
-    for (int i = 0; i < M; i++) {
-        if (join(edge[i].s, edge[i].e)) {
-            left--;
-            if (left == 1) {
-                cout << edge[i].t;
-                return 0;
-            }
-        }
-    }
-    cout << -1;
-    return 0;
+  }
+  cout << -1;
+  return 0;
 }
